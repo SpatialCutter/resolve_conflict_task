20c364ba81ebd186ab139f3826ba9c7b0f58dc21
SpatialCutter <spatialcutter@gmail.com>
2025-05-06 17:08:14 +0600

    base

---
+++ EcrypterApp/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace EcrypterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbFunction = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbKey = new System.Windows.Forms.NumericUpDown();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.bDecrypt = new System.Windows.Forms.Button();
+            this.bEncrypt = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbSource = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tbCypher = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tbKey)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbFunction
+            // 
+            this.cbFunction.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFunction.FormattingEnabled = true;
+            this.cbFunction.Items.AddRange(new object[] {
+            "Цезарь",
+            "Слово Цезаря",
+            "Транспонирование"});
+            this.cbFunction.Location = new System.Drawing.Point(138, 10);
+            this.cbFunction.Name = "cbFunction";
+            this.cbFunction.Size = new System.Drawing.Size(121, 21);
+            this.cbFunction.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Функция шифрования";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(282, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ключ шифрования";
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(388, 11);
+            this.tbKey.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(120, 20);
+            this.tbKey.TabIndex = 3;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(1, 41);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.bDecrypt);
+            this.splitContainer1.Panel1.Controls.Add(this.bEncrypt);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(798, 398);
+            this.splitContainer1.SplitterDistance = 423;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // bDecrypt
+            // 
+            this.bDecrypt.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.bDecrypt.Location = new System.Drawing.Point(344, 192);
+            this.bDecrypt.Name = "bDecrypt";
+            this.bDecrypt.Size = new System.Drawing.Size(76, 29);
+            this.bDecrypt.TabIndex = 1;
+            this.bDecrypt.Text = "< --";
+            this.bDecrypt.UseVisualStyleBackColor = true;
+            this.bDecrypt.Click += new System.EventHandler(this.bDecrypt_Click);
+            // 
+            // bEncrypt
+            // 
+            this.bEncrypt.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.bEncrypt.Location = new System.Drawing.Point(344, 157);
+            this.bEncrypt.Name = "bEncrypt";
+            this.bEncrypt.Size = new System.Drawing.Size(76, 29);
+            this.bEncrypt.TabIndex = 1;
+            this.bEncrypt.Text = "-- >";
+            this.bEncrypt.UseVisualStyleBackColor = true;
+            this.bEncrypt.Click += new System.EventHandler(this.bEncrypt_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.tbSource);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 392);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Исходный текст";
+            // 
+            // tbSource
+            // 
+            this.tbSource.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbSource.Location = new System.Drawing.Point(7, 20);
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Size = new System.Drawing.Size(322, 362);
+            this.tbSource.TabIndex = 0;
+            this.tbSource.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.tbCypher);
+            this.groupBox2.Location = new System.Drawing.Point(4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(364, 391);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Зашифрованный текст";
+            // 
+            // tbCypher
+            // 
+            this.tbCypher.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCypher.Location = new System.Drawing.Point(7, 19);
+            this.tbCypher.Name = "tbCypher";
+            this.tbCypher.Size = new System.Drawing.Size(351, 366);
+            this.tbCypher.TabIndex = 0;
+            this.tbCypher.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 438);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbFunction);
+            this.Name = "Form1";
+            this.Text = "EncypterApp";
+            ((System.ComponentModel.ISupportInitialize)(this.tbKey)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbFunction;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown tbKey;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button bDecrypt;
+        private System.Windows.Forms.Button bEncrypt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox tbSource;
+        private System.Windows.Forms.RichTextBox tbCypher;
+    }
+}
+
---
+++ EcrypterApp/IEncryptable.cs
@@ -0,0 +1,11 @@
+﻿namespace EcrypterApp
+{
+    //Интерфейс, указывающий на то, что класс является шифрующим
+    internal interface IEncryptable
+    {
+        //Метод шифрования
+        string Encrypt(string text, int key);
+        //Метод дешифрования
+        string Decrypt(string text, int key);
+    }
+}
